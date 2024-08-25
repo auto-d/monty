8868042794a06a62a75ff3a794e5e478b934ce81
auto-d <57813841+auto-d@users.noreply.github.com>
2024-08-25 17:36:07 -0400

    added some basic arg processing

--- monty.py
+++ monty.py
@@ -2,7 +2,7 @@ import time
 import argparse
 import agent
 
-def main(): 
+def main(args): 
     """
     Gardener agent entry point 
     """
@@ -10,13 +10,42 @@ def main():
     monty = agent.Gardener()
     monty.run()
 
+def validate_args(args): 
+    valid = False
+    error_prefix = "error: "
+    match args.backend: 
+        case "open-ai": 
+            if args.key and args.uri: 
+                valid = True 
+            else: 
+                print(error_prefix, "OpenAI backend requires a URI and key.")
+        case "ollama": 
+            if args.key and args.uri: 
+                valid = True 
+            else: 
+                print(error_prefix, "Ollama backend requires a URI and key.")
+        case "internal": 
+            if not args.key and not args.uri: 
+                valid = True
+            else: 
+                print(error_prefix, "URI or key provided but an internal model was requested!")
+
+    return valid
+
 if __name__ == '__main__': 
     parser = argparse.ArgumentParser(
         description="CLI for instantiating and interacting with the gardening agent."
         ) 
-    #parser.add_argument("--arg", help="an argument you can pass", type=int, choices=[1,2,3]) 
-    #parser.add_argument("-f", "--flag", help="a flag you can set", action="store_true")     
+    parser.add_argument("--backend", required=True, help="backend LLM to use for the agent",
+        choices=[
+            "open-ai", 
+            "ollama", 
+            "internal"
+        ]) 
+    parser.add_argument("--key", help="API key to use for LLM backend")
+    parser.add_argument("--uri", help="URI (ip:port/path) of LLM API")
     
     args = parser.parse_args() 
 
-    main() 
+    if validate_args(args):
+        main(args) 
